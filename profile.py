3a514693df8533d666aadcf682e5228d13ab7643
patrickmao <patrickmao@live.com>
2025-05-04 21:21:51 -0500

    working

--- profile.py
+++ profile.py
@@ -17,19 +17,9 @@ for i in range(NUM_NODES):
     lan.addInterface(iface)
 
     cmd = """
-#!/bin/bash
-set -e
-
-curl -L https://git.io/fFfOR | bash
-source ~/.profile
-
-TMHOME=/local/node{0}
-mkdir -p $TMHOME
-tendermint init --home $TMHOME
-
-NODE_ID=$(tendermint show_node_id --home $TMHOME)
-IP=$(hostname -I | awk '{{print $1}}')
-echo ">>> {1} ID=$NODE_ID at $IP:26656 <<<"
+sudo apt-get update
+sudo apt-get install -y golang
+export node_index={0}
 """.format(i, name)
 
     node.addService(rspec.Execute(shell="bash", command=cmd))
